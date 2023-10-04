e4ef3f7a57c9e6cd8a0a207b682fa23260d6101f
Abdulbasit <aabdulrafiu721@gmail.com>
2023-10-04 04:10:46 +0100

    style fixed

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,10 @@
+/**
+ *
+ * @param {*Array} studentList - an Array of students
+ * @param {*string} city - a string containing the desired student locations
+ * @param {*Array} newGrades - the grade to be attached t qualified student
+ * @returns - a new array modified based on the data given
+ */
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
   const defaultGrade = { grade: 'N/A' };
 
