df4c1c6b217d2cdc39a03dbfe2f4cb246ef1b716
Abdulbasit <aabdulrafiu721@gmail.com>
2023-10-06 18:36:14 +0100

    done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
   const resultArray = [];
-  if (!set || !(set instanceof Set) || !startString || typeof startString !== 'string') return '';
+  if (!set || !(set instanceof Set) || !startString || typeof startString !== 'string') return '\n';
   set.forEach((value) => {
-    if (value.startsWith(startString)) resultArray.push(value.slice(startString.length));
+    if (value.startsWith(startString)) resultArray.push(value.substring(startString.length));
   });
   return resultArray.join('-');
 }
