fd8076e50e733d5e588d3f1bdd9dc48b43706a73
Abdulbasit <aabdulrafiu721@gmail.com>
2023-10-06 18:51:25 +0100

    done

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,5 @@
 export default function updateUniqueItems(map) {
+  if (!map || !(map instanceof Map)) throw new Error('Cannot process');
   for (const [item, quantity] of map) {
     if (quantity === 1) {
       map.set(item, 100);
