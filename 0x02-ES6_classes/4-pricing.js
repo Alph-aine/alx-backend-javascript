c60ffd6e57244d6ecdcf9605f0427248f9b870f0
Abdulbasit <aabdulrafiu721@gmail.com>
2023-10-07 08:17:11 +0100

    done

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -3,6 +3,8 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
+    if (typeof ammount !== 'number') throw TypeError('Amount must be a number');
+    if (!(currency instanceof Currency)) throw TypeError('Currency must be a Currency');
     this._amount = amount;
     this._currency = currency;
   }
